6d2c53a35898494853e73ba5f90f6446dbcb8fd5
marleyanthony <marleycanthony@gmail.com>
2020-08-25 17:11:33 -0700

    updated inventory file. added first row of inventory items

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import InventoryItems from './components/InventoryItems'
 
 function App() {
   return (
-    <div>
-      <h1>Test</h1>
-    </div>
+    <>
+      <InventoryItems />
+    </>
   );
 }
 
