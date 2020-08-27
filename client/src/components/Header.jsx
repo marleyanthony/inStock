8c8a5db27846ee7f0fa7302dae7c343ea42aa7db
marleyanthony <marleycanthony@gmail.com>
2020-08-26 17:08:27 -0700

    Beginning of header. Added Router.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
-// import logo from '../assets/logo/logo';
+import logo from '../assets/Logo/logo.svg';
+import { Link } from 'react-router-dom';
 
 function header() {
   return (
     <section className="header">
       <div className="header__logo-container">
-        <img className="header__logo" src="./assets/logo/logo" alt="logo" />
+        <img className="header__logo" src={logo} alt="logo" />
       </div>
+
       <div className="header__wh-inv-container">
-        <button className="header__wh-btn btn">Warehouses</button>
-        <button className="header__inv-btn btn">Inventory</button>
+        <Link to="/warehouse">
+          <button className="header__wh-btn btn">Warehouses</button>
+        </Link>
+        <Link to="/inventory">
+          <button className="header__inv-btn btn">Inventory</button>
+        </Link>
       </div>
     </section>
   )
--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import App from './App';
+import InventoryItems from './components/InventoryItems';
+import Warehouses from './components/Warehouses';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Redirect path="/home" to="/" />
+        <Route path="/" component={App} exact />
+        <Route path="/inventory" component={InventoryItems} />
+        <Route path="/warehouse" component={Warehouses} />
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
