164182b8da9dbb8c82005ff8f04b03c5d521427d
pupocalypse <64297538+pupocalypse@users.noreply.github.com>
2020-08-27 15:53:04 -0700

    clean-up of index, App, Header from whatever merge error caused duplicating lines/older versions hanging around

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
@@ -7,30 +6,16 @@ import App from "./App";
 import Footer from "./components/Footer";
 
 import "./styles/app.css";
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import App from './App';
-import InventoryItems from './components/InventoryItems';
-import Warehouses from './components/Warehouses';
-import Header from './components/Header';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Header />
-
       <Route path="/" exact>
         <Redirect to="/warehouse" />
       </Route>
       <App />
       <Footer />
-      <Switch>
-        <Redirect path="/warehouse" to="/" />
-        <Route path="/" component={App} exact />
-        <Route path="/inventory" component={InventoryItems} />
-        {/* <Route path="/warehouse" component={Warehouses} /> */}
-      </Switch>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
