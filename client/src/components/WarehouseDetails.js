bb179ab5dd38e98910f526af2d4278de282d0ecd
pupocalypse <64297538+pupocalypse@users.noreply.github.com>
2020-08-28 13:31:44 -0700

    mobile styling for warehouse details page done

--- client/src/components/WarehouseDetails.js
+++ client/src/components/WarehouseDetails.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import backIcon from "../assets/Icons/arrow_back-24px.svg";
 import chevronIcon from "../assets/Icons/chevron_right-24px.svg";
@@ -11,11 +12,13 @@ const WarehouseDetails = (props) => {
     <main className="warehouse">
       <section className="warehouse__header-container">
         <div className="warehouse__back-name">
-          <img
-            src={backIcon}
-            alt=""
-            className="warehouse__icon warehouse__icon--back"
-          />
+          <Link to="/warehouse">
+            <img
+              src={backIcon}
+              alt=""
+              className="warehouse__icon warehouse__icon--back"
+            />
+          </Link>
           <h1 className="warehouse__heading">warehouse name</h1>
         </div>
         <button className="warehouse__button">
@@ -49,24 +52,35 @@ const WarehouseDetails = (props) => {
       <section className="warehouse__inv-list">
         {/* this will be populated by another map() */}
         <div className="warehouse__inv-item">
-          <p className="warehouse__label">INVENTORY ITEM</p>
-          <a>
-            <h3 className="warehouse__text">Item</h3>
-            <img
-              src={chevronIcon}
-              alt=""
-              className="warehouses__icon warehouses__icon--chevron"
-            />
-          </a>
+          <div className="warehouse__left-container">
+            <p className="warehouse__label">INVENTORY ITEM</p>
+            <a className="warehouse__link">
+              <h3 className="warehouse__text warehouse__text--link">Item</h3>
+              <img
+                src={chevronIcon}
+                alt=""
+                className="warehouses__icon warehouses__icon--chevron"
+              />
+            </a>
 
-          <p className="warehouse__label">CATEGORY</p>
-          <h3 className="warehouse__text">Category</h3>
+            <p className="warehouse__label">CATEGORY</p>
+            <h3 className="warehouse__text">Category</h3>
+          </div>
 
-          <p className="warehouse__label">Status</p>
-          <h3 className="warehouse__text">In Stock</h3>
+          <div className="warehouse__right-container">
+            <p className="warehouse__label">STATUS</p>
+            <h3 className="warehouse__text warehouse__text--status">
+              In Stock
+            </h3>
 
-          <p className="warehouse__label">QTY</p>
-          <h3 className="warehouse__text">0</h3>
+            <p className="warehouse__label">QTY</p>
+            <h3 className="warehouse__text">0</h3>
+          </div>
+
+          <div className="warehouse__icons-container">
+            <img src={deleteIcon} alt="" className="warehouse__icon" />
+            <img src={editIcon} alt="" className="warehouse__icon" />
+          </div>
         </div>
       </section>
     </main>
