c3bef3b9b886ac4d1fc9899701a34618d73b0729
Sabzs <sajiyoonline@gmail.com>
2020-08-29 14:36:52 -0700

    finished styling desktop breakpoint in newWarehouse.scss

--- client/src/components/newWarehouse.js
+++ client/src/components/newWarehouse.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 // import { Link } from "react-router-dom";
 import arrowBack from "../assets/Icons/arrow_back-24px.svg";
+import errorIcon from "../assets/Icons/error-24px.svg";
 
 class newWarehouse extends Component {
     render() {
@@ -11,29 +12,53 @@ class newWarehouse extends Component {
                     <img src={arrowBack} alt="Back Arrow" />
                     <h1 className="newWarehouse__wh-heading">Add New Warehouse</h1>
                 </div>
-                <div className="newWarehouse__tabletDesk">
+                <div className="newWarehouse__details-contacts-container">
                     <div className="newWarehouse__wh-details-container">
                         <h3 className="newWarehouse__details-heading">Warehouse Details</h3>
                         <p className="newWarehouse__wh-label">Warehouse Name</p>
-                        <input className="newWarehouse__input-field" type="text" placeholder="Warehouse Name" />
+                        <input className="newWarehouse__input-field newWarehouse__input-field--tabDesk-det" type="text" placeholder="Warehouse Name" />
+                        <div className="newWarehouse__errorRequired-container">
+                            <img src={errorIcon} alt="error icon" /> <p>This field is required</p>
+                        </div>
                         <p className="newWarehouse__wh-label">Street Name</p>
-                        <input className="newWarehouse__input-field" type="text" placeholder="Street Name" />
+                        <input className="newWarehouse__input-field newWarehouse__input-field--tabDesk-det" type="text" placeholder="Street Name" />
+                        <div className="newWarehouse__errorRequired-container">
+                            <img src={errorIcon} alt="error icon" /> <p>This field is required</p>
+                        </div>
                         <p className="newWarehouse__wh-label">City</p>
-                        <input className="newWarehouse__input-field" type="text" placeholder="City" />
+                        <input className="newWarehouse__input-field newWarehouse__input-field--tabDesk-det" type="text" placeholder="City" />
+                        <div className="newWarehouse__errorRequired-container">
+                            <img src={errorIcon} alt="error icon" /> <p>This field is required</p>
+                        </div>
                         <p className="newWarehouse__wh-label">Country</p>
-                        <input className="newWarehouse__input-field" type="text" placeholder="Country" />
+                        <input className="newWarehouse__input-field newWarehouse__input-field--tabDesk-det" type="text" placeholder="Country" />
+                        <div className="newWarehouse__errorRequired-container">
+                            <img src={errorIcon} alt="error icon" /> <p>This field is required</p>
+                        </div>
                     </div>
 
                     <div className="newWarehouse__wh-contacts-container">
                         <h3 className="newWarehouse__contacts-heading">Contact Details</h3>
                         <p className="newWarehouse__wh-label">Contact Name</p>
-                        <input className="newWarehouse__input-field" type="text" placeholder="Contact Number" />
+                        <input className="newWarehouse__input-field newWarehouse__input-field--tabDesk-cont" type="text" placeholder="Contact Number" />
+                        <div className="newWarehouse__errorRequired-container">
+                            <img src={errorIcon} alt="error icon" /> <p>This field is required</p>
+                        </div>
                         <p className="newWarehouse__wh-label">Position</p>
-                        <input className="newWarehouse__input-field" type="text" placeholder="Position" />
+                        <input className="newWarehouse__input-field newWarehouse__input-field--tabDesk-cont" type="text" placeholder="Position" />
+                        <div className="newWarehouse__errorRequired-container">
+                            <img src={errorIcon} alt="error icon" /> <p>This field is required</p>
+                        </div>
                         <p className="newWarehouse__wh-label">Phone Number</p>
-                        <input className="newWarehouse__input-field" type="text" placeholder="Phone Number" />
+                        <input className="newWarehouse__input-field newWarehouse__input-field--tabDesk-cont" type="text" placeholder="Phone Number" />
+                        <div className="newWarehouse__errorRequired-container">
+                            <img src={errorIcon} alt="error icon" /> <p>This field is required</p>
+                        </div>
                         <p className="newWarehouse__wh-label">Email</p>
-                        <input className="newWarehouse__input-field" type="text" placeholder="Email" />
+                        <input className="newWarehouse__input-field newWarehouse__input-field--tabDesk-cont" type="text" placeholder="Email" />
+                        <div className="newWarehouse__errorRequired-container">
+                            <img src={errorIcon} alt="error icon" /> <p>This field is required</p>
+                        </div>
                     </div>
                 </div>
 
