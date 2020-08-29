cb1531b28a44525c3d8a6d6acbf5fbbd4c2d8f83
marleyanthony <marleycanthony@gmail.com>
2020-08-29 12:13:03 -0700

    updated inventory component styling

--- client/src/components/InventoryItems.js
+++ client/src/components/InventoryItems.js
@@ -12,32 +12,44 @@ const InventoryItems = ({ inventories }) => {
     return (
       <div className="inventory__item">
         <div className="inventory__item-left-container">
-          <p className="inventory__label">Inventory Item</p>
-          {/* <Link to={`/warehouse/${navLink}`} className="inventory__link"> */}
-          <h3 className="inventory__text-location">
+          <p className="inventory__label">
+            Inventory Item
+          </p>
+          <h3 className="inventory__item-name">
             {inventory.item}
           </h3>
-          <img
-            src={chevronIcon}
-            alt=""
-            className="inventory__icon inventory__icon--chevron"
-          />
-          {/* </Link> */}
-          <p className="inventory__label">Category</p>
-          <p className="inventory__text-address">
+          <img src={chevronIcon} alt="" className="inventory__icon inventory__icon--chevron" />
+          <p className="inventory__label">
+            Category
+          </p>
+          <p className="inventory__category">
             {inventory.category}
           </p>
         </div>
 
         <div className="inventory__item-right-container">
-          <p className="inventory__label">Status</p>
-          <p className="inventory__text-contact-name">
-            {inventory.status}
-          </p>
+          <div className="inventory__status-container">
+            <p className="inventory__label">
+              Status
+            </p>
+            <p className="inventory__status">
+              {inventory.status}
+            </p>
+          </div>
           <div className="inventory__contact-info-container">
-            <p className="inventory__label">QTY</p>
-            <p className="inventory__text-contact-phone">
+            <p className="inventory__label">
+              QTY
+            </p>
+            <p className="inventory__quantity">
               {inventory.quantity}
+            </p>
+          </div>
+          <div className="inventory__contact-info-container">
+            <p className="inventory__label">
+              Warehouse
+            </p>
+            <p className="inventory__warehouse">
+              {inventory.warehouse}
             </p>
           </div>
         </div>
@@ -51,20 +63,17 @@ const InventoryItems = ({ inventories }) => {
   });
 
   return (
-    <main className="warehouses">
-      <section className="warehouses__header-container">
-        <h1 className="warehouses__heading">Warehouses</h1>
-        <div className="warehouses__search-add">
-          <label htmlFor="search" className="warehouses__search-label">
-            <input
-              type="search"
-              className="warehouses__searchbar"
-              name="search"
-              placeholder="Search..."
-            />
-            <img src={searchIcon} alt="" className="warehouses__search-icon" />
+    <main className="inventory">
+      <section className="inventory__header-container">
+        <h1 className="inventory__heading">
+          Inventory
+        </h1>
+        <div className="inventory__search-add">
+          <label htmlFor="search" className="inventory__search-label">
+            <input type="search" className="inventory__searchbar" name="search" placeholder="Search..." />
+            <img src={searchIcon} alt="" className="inventory__search-icon" />
           </label>
-          <button className="warehouses__button">+ Add New Warehouse</button>
+          <button className="inventory__button">+ Add New Item</button>
         </div>
       </section>
 
@@ -72,49 +81,41 @@ const InventoryItems = ({ inventories }) => {
         <div className="inventory__sort">
           <div className="inventory__sort-left">
             <div className="inventory__sort-category sort-warehouse">
-              <p className="inventory__sort-label">Inventory Item</p>
-              <img
-                src={sortIcon}
-                alt=""
-                className="inventory__icon inventory__icon--sort"
-              />
+              <p className="inventory__sort-label">
+                Inventory Item
+              </p>
+              <img src={sortIcon} alt="" className="inventory__icon inventory__icon--sort" />
             </div>
             <div className="inventory__sort-category sort-address">
-              <p className="inventory__sort-label">Category</p>
-              <img
-                src={sortIcon}
-                alt=""
-                className="inventory__icon inventory__icon--sort"
-              />
+              <p className="inventory__sort-label">
+                Category
+              </p>
+              <img src={sortIcon} alt="" className="inventory__icon inventory__icon--sort" />
             </div>
           </div>
           <div className="inventory__sort-right">
             <div className="inventory__sort-category sort-name">
-              <p className="inventory__sort-label">Status</p>
-              <img
-                src={sortIcon}
-                alt=""
-                className="inventory__icon inventory__icon--sort"
-              />
+              <p className="inventory__sort-label">
+                Status
+              </p>
+              <img src={sortIcon} alt="" className="inventory__icon inventory__icon--sort" />
             </div>
             <div className="inventory__sort-category sort-info">
-              <p className="inventory__sort-label">QTY</p>
-              <img
-                src={sortIcon}
-                alt=""
-                className="inventory__icon inventory__icon--sort"
-              />
+              <p className="inventory__sort-label">
+                QTY
+              </p>
+              <img src={sortIcon} alt="" className="inventory__icon inventory__icon--sort" />
             </div>
             <div className="inventory__sort-category sort-info">
-              <p className="inventory__sort-label">Warehouse</p>
-              <img
-                src={sortIcon}
-                alt=""
-                className="inventory__icon inventory__icon--sort"
-              />
+              <p className="inventory__sort-label">
+                Warehouse
+              </p>
+              <img src={sortIcon} alt="" className="inventory__icon inventory__icon--sort" />
             </div>
           </div>
-          <p className="inventory__sort-label sort-actions">ACTIONS</p>
+          <p className="inventory__sort-label sort-actions">
+            ACTIONS
+          </p>
         </div>
         {inventoryList}
       </section>
