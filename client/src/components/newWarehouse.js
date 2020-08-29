8e0df4abded09819ba668b6cda87f34f7a809b57
Sabzs <sajiyoonline@gmail.com>
2020-08-29 00:30:04 -0700

    added newWarehouse partials and styled most of mobile breakpoint

--- client/src/components/newWarehouse.js
+++ client/src/components/newWarehouse.js
@@ -1,36 +1,44 @@
 import React, { Component } from "react";
 // import { Link } from "react-router-dom";
+import arrowBack from "../assets/Icons/arrow_back-24px.svg";
 
 class newWarehouse extends Component {
     render() {
         return (
             <form className="newWarehouse">
-                <h1 className="newWarehouse__heading">Add New Warehouse</h1>
-                <div className="newWarehouse__detials">
-                    <h1 className="newWarehouse__details-heading">Warehouse Details</h1>
-                    <p className="newWarehouse__label">Warehouse Name</p>
+
+                <div className="newWarehouse__arr-heading-container">
+                    <img src={arrowBack} alt="Back Arrow" />
+                    <h1 className="newWarehouse__wh-heading">Add New Warehouse</h1>
+                </div>
+
+                <div className="newWarehouse__wh-details-container">
+                    <h3 className="newWarehouse__details-heading">Warehouse Details</h3>
+                    <p className="newWarehouse__wh-label">Warehouse Name</p>
                     <input className="newWarehouse__input-field" type="text" placeholder="Warehouse Name" />
-                    <p className="newWarehouse__label">Street Name</p>
+                    <p className="newWarehouse__wh-label">Street Name</p>
                     <input className="newWarehouse__input-field" type="text" placeholder="Street Name" />
-                    <p className="newWarehouse__label">City</p>
+                    <p className="newWarehouse__wh-label">City</p>
                     <input className="newWarehouse__input-field" type="text" placeholder="City" />
-                    <p className="newWarehouse__label">Country</p>
+                    <p className="newWarehouse__wh-label">Country</p>
                     <input className="newWarehouse__input-field" type="text" placeholder="Country" />
                 </div>
 
-                <div className="newWarehouse__contacts">
-                    <h1 className="newWarehouse__contacts-heading">Contact Details</h1>
-                    <p className="newWarehouse__label">Contact Name</p>
+                <div className="newWarehouse__wh-contacts-container">
+                    <h3 className="newWarehouse__contacts-heading">Contact Details</h3>
+                    <p className="newWarehouse__wh-label">Contact Name</p>
                     <input className="newWarehouse__input-field" type="text" placeholder="Contact Number" />
-                    <p className="newWarehouse__label">Position</p>
+                    <p className="newWarehouse__wh-label">Position</p>
                     <input className="newWarehouse__input-field" type="text" placeholder="Position" />
-                    <p className="newWarehouse__label">Phone Number</p>
+                    <p className="newWarehouse__wh-label">Phone Number</p>
                     <input className="newWarehouse__input-field" type="text" placeholder="Phone Number" />
-                    <p className="newWarehouse__label">Email</p>
+                    <p className="newWarehouse__wh-label">Email</p>
                     <input className="newWarehouse__input-field" type="text" placeholder="Email" />
                 </div>
 
                 <div className="newWarehouse__CTAs">
+                    <button className="newWarehouse__cancel-btn" >Cancel</button>
+                    <button className="newWarehouse__add-wh-btn" >+ Add Warehouse</button>
 
                 </div>
             </form>
