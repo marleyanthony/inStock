fefdc2782ec5991bb6f4f0486fd3093c6b7312b2
Sabzs <sajiyoonline@gmail.com>
2020-08-30 11:59:11 -0700

    accepted and committed incoming changers

---
+++ client/src/components/editWarehouse.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+// import React from "react";
+// import { Link } from "react-router-dom";
+import arrowBack from "../assets/Icons/arrow_back-24px.svg";
+
+
+class editWarehouse extends Component {
+
+
+    render() {
+        return (
+            <form className="newWarehouse" onSubmit={this.handleSubmit}>
+
+            </form>
+
+        )
+    }
+
+}
+
+export default editWarehouse; 
